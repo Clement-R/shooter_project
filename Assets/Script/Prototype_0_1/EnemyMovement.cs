1526f37760f07152ce9a0215b2ef5b5c6373b250
C.Rondeau <rondeau.clement@gmail.com>
2016-11-02 23:35:09 +0100

    Add enemy spawn and movement

---
+++ Assets/Script/Prototype_0_1/EnemyMovement.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyMovement : MonoBehaviour {
+    public int speed;
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 move = new Vector3(0, 1, 0);
+        transform.position += move * -speed * Time.deltaTime;
+    }
+}
