44d78db684daf2bf8baf3b9c2c15f64501a4a3b1
Clement-R <rondeau.clement@gmail.com>
2016-11-03 16:31:13 +0100

    Add sound on bullet hit wall and enemy death

--- Assets/Script/Prototype_0_1/EnemyBehavior.cs
+++ Assets/Script/Prototype_0_1/EnemyBehavior.cs
@@ -2,9 +2,20 @@
 using System.Collections;
 
 public class EnemyBehavior : MonoBehaviour {
+    public GameObject deathSound;
 
-	// Use this for initialization
-	void Start () {
-        Destroy(this, 3.0f);
+    // Use this for initialization
+    void Start () {
+        Destroy(this.gameObject, 12.0f);
 	}
+
+    public void Die()
+    {
+        // Play sound
+        GameObject deathSoundEmitter = Instantiate(deathSound);
+        deathSoundEmitter.transform.position = gameObject.transform.position;
+        
+        // Destroy game object
+        Destroy(this.gameObject);
+    }
 }
