a552e925e22d128dd8362ee63d80caa86492f79d
C.Rondeau <rondeau.clement@gmail.com>
2016-11-07 23:24:13 +0100

    Add target system for the fennec

--- Assets/Script/Prototype_1/EnemyBehavior.cs
+++ Assets/Script/Prototype_1/EnemyBehavior.cs
@@ -3,10 +3,37 @@ using System.Collections;
 
 public class EnemyBehavior : MonoBehaviour {
     public GameObject deathSound;
+    public bool isTargeted = false;
 
     void Start () {
         Destroy(this.gameObject, 12.0f);
 	}
+
+    void Update()
+    {
+        if (this.isTargeted)
+        {
+            StartCoroutine("effect");
+        }
+        else
+        {
+            StopCoroutine("effect");
+        }
+    }
+
+    public IEnumerator effect()
+    {
+        if(GetComponent<SpriteRenderer>().enabled == false)
+        {
+            GetComponent<SpriteRenderer>().enabled = true;
+        }
+        else
+        {
+            GetComponent<SpriteRenderer>().enabled = false;
+        }
+        
+        yield return new WaitForSeconds(0.25f);
+    }
 
     public void Die()
     {
--- Assets/Script/Prototype_1/ShuttleSoundEmitter.cs
+++ Assets/Script/Prototype_1/ShuttleSoundEmitter.cs
@@ -2,20 +2,45 @@
 using System.Collections;
 
 public class ShuttleSoundEmitter : MonoBehaviour {
-
     public GameObject spawnOnLeftClick;
-    public GameObject spawnOnRightClick;
+    public float minRange;
+
+    private GameObject targetedEnemy = null;
+    private float closestDistance = 1000.0f;
     
-    // Update is called once per frame
     void Update() {
         if (Input.GetMouseButtonDown(0)) {
+            // Unmark last enemy if needed
+            if(targetedEnemy != null)
+            {
+                targetedEnemy.GetComponent<EnemyBehavior>().isTargeted = false;
+                targetedEnemy = null;
+                closestDistance = 1000.0f;
+            }
+
+            // Find all enemies
+            GameObject[] enemies = GameObject.FindGameObjectsWithTag("enemy");
+            foreach (GameObject enemy in enemies) {
+                // Calculate distance between Fennec and the enemy
+                float distance = Vector2.Distance(this.transform.position, enemy.transform.position);
+
+                // Keep the enemy if the distance is inferior than the saved distance
+                if(distance < closestDistance && distance < minRange)
+                {
+                    closestDistance = distance;
+                    targetedEnemy = enemy;
+                }
+            }
+
+            // Mark closest enemy
+            if (targetedEnemy != null) {
+                targetedEnemy.GetComponent<EnemyBehavior>().isTargeted = true;
+            }
+            
+
+            // Play sound effect
             GameObject explosion = Instantiate(spawnOnLeftClick);
             explosion.transform.position = gameObject.transform.position;
-        }
-
-        if (Input.GetMouseButtonDown(1)) {
-            GameObject laser = Instantiate(spawnOnRightClick);
-            laser.transform.position = gameObject.transform.position; ;
         }
     }
 }
