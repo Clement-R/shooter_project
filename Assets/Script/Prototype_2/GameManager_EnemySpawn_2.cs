5ecb05764a21651d995f129ec12629299803e155
Clement-R <rondeau.clement@gmail.com>
2016-11-07 16:20:12 +0100

    Add new spawn method to spawn enemies one by one

---
+++ Assets/Script/Prototype_2/GameManager_EnemySpawn_2.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager_EnemySpawn : MonoBehaviour {
+    public GameObject Enemy;
+    public int timeBetweenSpawn = 1;
+
+    // Update is called once per frame
+    void Start() {
+        StartCoroutine("spawnEnemy");
+    }
+
+    IEnumerator spawnEnemy() {
+        Instantiate(Enemy, new Vector3(Random.Range(-4.0f, 4.0f), 5.4f, 0), Quaternion.identity);
+        yield return new WaitForSeconds(timeBetweenSpawn);
+        StartCoroutine("spawnEnemy");
+    }
+}
