c95bfc3ecb7571e8607b547866ee031fff774525
C.Rondeau <rondeau.clement@gmail.com>
2016-11-08 22:28:18 +0100

    Start to work on wave system

--- Assets/Script/Prototype_2/GameManager_EnemySpawn_2.cs
+++ Assets/Script/Prototype_2/GameManager_EnemySpawn_2.cs
@@ -5,18 +5,19 @@ public class GameManager_EnemySpawn_2 : MonoBehaviour {
     public GameObject Enemy;
 
     private GameObject actualEnemy;
+    private GameManager_WaveSpawner spawner;
 
     void Start() {
-        actualEnemy = spawnEnemy();
+        spawner = GetComponent<GameManager_WaveSpawner>();
     }
 
-    GameObject spawnEnemy() {
+    GameObject spawnEnemy(float x, float y) {
         return Instantiate(Enemy, new Vector3(Random.Range(-4.0f, 4.0f), 5.4f, 0), Quaternion.identity) as GameObject;
     }
 
     void Update() {
         if (actualEnemy == null) {
-            actualEnemy = spawnEnemy();
+            // spawner.nextWave();
         }
     }
 }
---
+++ Assets/Script/Prototype_2/GameManager_WaveSpawner.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager_WaveSpawner : MonoBehaviour
+{
+    private int numberOfEnemiesInWave = 0;
+    private int numberOfEnemiesKilled = 0;
+    private int currentWave = 0;
+
+    void Start ()
+    {
+        //Load data from disk
+        TextAsset wavesData = Resources.Load("waves") as TextAsset;
+        Debug.Log(wavesData.text);
+
+        /*
+        map<String><map<int><float>>
+        0 :
+            1, 1.0f
+            1, 2.0f
+        1:
+            1, 3.0f
+            2, 2.0f
+        */
+
+        // Save data in a map of map
+        //
+        // Reverse the array
+        //
+
+        sendWave();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    if(numberOfEnemiesInWave == numberOfEnemiesKilled)
+        {
+            currentWave ++;
+            // Send next wave
+        }
+    }
+
+    void sendWave()
+    {
+    }
+}
