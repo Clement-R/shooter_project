fc1ee46c0a3f37882f756daf42d70d61ef714f3b
thomasfrancisco <thofrancisco@gmail.com>
2016-11-24 20:27:06 +0100

    Vibration controlled by controller's trigger

--- Assets/Script/rumble/Rumble.cs
+++ Assets/Script/rumble/Rumble.cs
@@ -11,6 +11,7 @@ public class Rumble : MonoBehaviour {
     private PlayerIndex player2;
 
     private Hashtable rumblesDuration; //Permet de connaitre le temps restant pour faire vibrer chaque moteur;
+    private Hashtable rumblesStrength;
 
 	// Use this for initialization
 	void Start () {
@@ -21,6 +22,12 @@ public class Rumble : MonoBehaviour {
         rumblesDuration.Add(MotorName.leftP2, 0.0f);
         rumblesDuration.Add(MotorName.rightP1, 0.0f);
         rumblesDuration.Add(MotorName.rightP2, 0.0f);
+
+        rumblesStrength = new Hashtable();
+        rumblesStrength.Add(MotorName.leftP1, 0.0f);
+        rumblesStrength.Add(MotorName.leftP2, 0.0f);
+        rumblesStrength.Add(MotorName.rightP1, 0.0f);
+        rumblesStrength.Add(MotorName.rightP2, 0.0f);
 
     }
 	
@@ -36,16 +43,16 @@ public class Rumble : MonoBehaviour {
 
         if((float)rumblesDuration[MotorName.leftP1] > 0.0f && (float)rumblesDuration[MotorName.rightP1] > 0.0f)
         {
-            GamePad.SetVibration(player1, 1.0f, 1.0f);
+            GamePad.SetVibration(player1, (float)rumblesStrength[MotorName.leftP1], (float)rumblesStrength[MotorName.rightP1]);
             rumblesDuration[MotorName.leftP1] = (float)rumblesDuration[MotorName.leftP1] - Time.deltaTime;
             rumblesDuration[MotorName.rightP1] = (float)rumblesDuration[MotorName.rightP1] - Time.deltaTime;
         } else if((float)rumblesDuration[MotorName.leftP1] > 0.0f && (float)rumblesDuration[MotorName.rightP1] <= 0.0f)
         {
-            GamePad.SetVibration(player1, 1.0f, 0.0f);
+            GamePad.SetVibration(player1, (float)rumblesStrength[MotorName.leftP1], 0.0f);
             rumblesDuration[MotorName.leftP1] = (float)rumblesDuration[MotorName.leftP1] - Time.deltaTime;
         } else if ((float)rumblesDuration[MotorName.leftP1] <= 0.0f && (float)rumblesDuration[MotorName.rightP1] > 0.0f)
         {
-            GamePad.SetVibration(player1, 0.0f, 1.0f);
+            GamePad.SetVibration(player1, 0.0f, (float)rumblesStrength[MotorName.rightP1]);
             rumblesDuration[MotorName.rightP1] = (float)rumblesDuration[MotorName.rightP1] - Time.deltaTime;
         } else if ((float)rumblesDuration[MotorName.leftP1] <= 0.0f && (float)rumblesDuration[MotorName.rightP1] <= 0.0f)
         {
@@ -55,18 +62,18 @@ public class Rumble : MonoBehaviour {
 
         if ((float)rumblesDuration[MotorName.leftP2] > 0.0f && (float)rumblesDuration[MotorName.rightP2] > 0.0f)
         {
-            GamePad.SetVibration(player2, 1.0f, 1.0f);
+            GamePad.SetVibration(player2, (float)rumblesStrength[MotorName.leftP2], (float)rumblesStrength[MotorName.rightP2]);
             rumblesDuration[MotorName.leftP2] = (float)rumblesDuration[MotorName.leftP2] - Time.deltaTime;
             rumblesDuration[MotorName.rightP2] = (float)rumblesDuration[MotorName.rightP2] - Time.deltaTime;
         }
         else if ((float)rumblesDuration[MotorName.leftP2] > 0.0f && (float)rumblesDuration[MotorName.rightP2] <= 0.0f)
         {
-            GamePad.SetVibration(player2, 1.0f, 0.0f);
+            GamePad.SetVibration(player2, (float)rumblesStrength[MotorName.leftP2], 0.0f);
             rumblesDuration[MotorName.leftP2] = (float)rumblesDuration[MotorName.leftP2] - Time.deltaTime;
         }
         else if ((float)rumblesDuration[MotorName.leftP2] <= 0.0f && (float)rumblesDuration[MotorName.rightP2] > 0.0f)
         {
-            GamePad.SetVibration(player2, 0.0f, 1.0f);
+            GamePad.SetVibration(player2, 0.0f, (float)rumblesStrength[MotorName.rightP2]);
             rumblesDuration[MotorName.rightP2] = (float)rumblesDuration[MotorName.rightP2] - Time.deltaTime;
         }
         else if ((float)rumblesDuration[MotorName.leftP2] <= 0.0f && (float)rumblesDuration[MotorName.rightP2] <= 0.0f)
@@ -78,8 +85,9 @@ public class Rumble : MonoBehaviour {
 
     }
 
-    public void rumble(MotorName name)
+    public void rumble(MotorName name, float strength)
     {
         rumblesDuration[name] = durationInSeconds;
+        rumblesStrength[name] = strength;
     }
 }
--- Assets/Script/rumble/VibrationExample.cs
+++ Assets/Script/rumble/VibrationExample.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using XInputDotNetPure;
 
 public class VibrationExample : MonoBehaviour {
 
@@ -12,13 +13,30 @@ public class VibrationExample : MonoBehaviour {
 	void Update () {
         if (Input.GetMouseButtonDown(0))
         {
-            gameObject.GetComponent<Rumble>().rumble(Rumble.MotorName.leftP1);
+            gameObject.GetComponent<Rumble>().rumble(Rumble.MotorName.leftP1, 1.0f);
         }
         if (Input.GetMouseButtonDown(1))
         {
-            gameObject.GetComponent<Rumble>().rumble(Rumble.MotorName.rightP1);
+            gameObject.GetComponent<Rumble>().rumble(Rumble.MotorName.rightP1, 1.0f);
         }
         
-	
-	}
+        if(GamePad.GetState((PlayerIndex)0).Triggers.Left > 0)
+        {
+            gameObject.GetComponent<Rumble>().rumble(Rumble.MotorName.leftP2, GamePad.GetState((PlayerIndex)0).Triggers.Left);
+        }
+        if (GamePad.GetState((PlayerIndex)0).Triggers.Right > 0)
+        {
+            gameObject.GetComponent<Rumble>().rumble(Rumble.MotorName.rightP2, GamePad.GetState((PlayerIndex)0).Triggers.Right);
+        }
+
+        if (GamePad.GetState((PlayerIndex)1).Triggers.Left > 0)
+        {
+            gameObject.GetComponent<Rumble>().rumble(Rumble.MotorName.leftP1, GamePad.GetState((PlayerIndex)1).Triggers.Left);
+        }
+        if (GamePad.GetState((PlayerIndex)1).Triggers.Right > 0)
+        {
+            gameObject.GetComponent<Rumble>().rumble(Rumble.MotorName.rightP1, GamePad.GetState((PlayerIndex)1).Triggers.Right);
+        }
+
+    }
 }
