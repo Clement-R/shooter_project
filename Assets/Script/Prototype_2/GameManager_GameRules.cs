8deb4598e1e351679e0c428fad1e2a05469ebe9e
thomasfrancisco <thofrancisco@gmail.com>
2016-11-24 21:09:29 +0100

    Implement public method for game managing

--- Assets/Script/Prototype_2/GameManager_GameRules.cs
+++ Assets/Script/Prototype_2/GameManager_GameRules.cs
@@ -5,13 +5,26 @@ public class GameManager_GameRules : MonoBehaviour
 {
     bool gameLost = false;
 
+
+    public int lifeInit;
+    [Tooltip("Points obtenu lorsqu'un ennemi est vaincu")]
+    public int pointPerHit;
+    [Tooltip("Points perdu lorsqu'un ennemi a traversé l'écran")]
+    public int malus;
+
+    private int life;
+    private int score;
+    
+
 	void Start ()
     {
+        life = lifeInit;
+        score = 0;
 	}
 	
 	void Update ()
     {
-	    if(gameLost)
+	    if(life < 0 || gameLost)
         {
             Time.timeScale = 0.0f;
         }
@@ -21,4 +34,24 @@ public class GameManager_GameRules : MonoBehaviour
     {
         this.gameLost = true;
     }
+
+    public void ennemyShot()
+    {
+        score += pointPerHit;
+    }
+
+    public void playerTouched()
+    {
+        life--;
+    }
+
+    public void ennemyPassed()
+    {
+        score -= malus;
+        if(score < 0)
+        {
+            score = 0;
+        }
+    }
+    
 }
