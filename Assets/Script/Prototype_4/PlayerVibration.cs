612448702b9f923648add162375a517db7e678c1
Clement-R <rondeau.clement@gmail.com>
2016-11-27 22:21:11 +0100

    Add rumble communication between players

---
+++ Assets/Script/Prototype_4/PlayerVibration.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using XInputDotNetPure;
+
+public class PlayerVibration : MonoBehaviour {
+
+	void Update () {
+        if (GamePad.GetState((PlayerIndex)0).Triggers.Left > 0) {
+            gameObject.GetComponent<Rumble>().rumble(Rumble.MotorName.leftP2, GamePad.GetState((PlayerIndex)0).Triggers.Left);
+        }
+        if (GamePad.GetState((PlayerIndex)0).Triggers.Right > 0) {
+            gameObject.GetComponent<Rumble>().rumble(Rumble.MotorName.rightP2, GamePad.GetState((PlayerIndex)0).Triggers.Right);
+        }
+
+        if (GamePad.GetState((PlayerIndex)1).Triggers.Left > 0) {
+            gameObject.GetComponent<Rumble>().rumble(Rumble.MotorName.leftP1, GamePad.GetState((PlayerIndex)1).Triggers.Left);
+        }
+        if (GamePad.GetState((PlayerIndex)1).Triggers.Right > 0) {
+            gameObject.GetComponent<Rumble>().rumble(Rumble.MotorName.rightP1, GamePad.GetState((PlayerIndex)1).Triggers.Right);
+        }
+    }
+}
