fd82f7eabf81a615955117654b0fc89b487b0ce9
thomasfrancisco <thofrancisco@gmail.com>
2016-11-28 12:35:58 +0100

    GrandMa and fox sprite integration (no anim)

---
+++ Assets/Script/master/turretBehavior_master.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class turretBehavior_master : MonoBehaviour {
+
+    private Rigidbody2D rb2D;
+
+    void Start()
+    {
+        rb2D = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        var angle = Mathf.Atan2(Input.GetAxisRaw("Horizontal_2"), -Input.GetAxisRaw("Vertical_2")) * Mathf.Rad2Deg;
+
+        // QUICKFIX - Angle = 180 if there is no input
+        if (angle == 180)
+        {
+            angle = 0;
+        }
+        if(angle > 90)
+        {
+            angle = 90;
+        }
+        if(angle < -90)
+        {
+            angle = -90;
+        }
+
+        transform.FindChild("pivotScope").transform.localEulerAngles = new Vector3(0, 0, -angle);
+    }
+}
