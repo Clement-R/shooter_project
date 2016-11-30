15e18ff30e6b5b961945fe9814fb673d2814c364
thomasfrancisco <thofrancisco@gmail.com>
2016-11-30 14:13:30 +0100

    Manage Trash collision with fox

---
+++ Assets/Script/master/ShuttleBehavior_master.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShuttleBehavior_master : MonoBehaviour {
+    public bool isStuned = false;
+    public float stunCooldown;
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.gameObject.tag == "trash")
+        {
+            if (!isStuned)
+            {
+                isStuned = true;
+                StartCoroutine("stunEffect");
+                coll.gameObject.GetComponent<TrashBehavior_master>().Die();
+            }
+        }
+    }
+
+    void Update()
+    {
+        if (this.isStuned)
+        {
+            StartCoroutine("effect");
+        }
+        else
+        {
+            StopCoroutine("effect");
+            GetComponent<SpriteRenderer>().enabled = true;
+        }
+    }
+
+    public IEnumerator stunEffect()
+    {
+        yield return new WaitForSeconds(stunCooldown);
+        isStuned = false;
+        GetComponent<SpriteRenderer>().enabled = true;
+    }
+
+    public IEnumerator effect()
+    {
+        if (GetComponent<SpriteRenderer>().enabled == false)
+        {
+            GetComponent<SpriteRenderer>().enabled = true;
+        }
+        else
+        {
+            GetComponent<SpriteRenderer>().enabled = false;
+        }
+
+        yield return new WaitForSeconds(0.25f);
+        StartCoroutine("effect");
+    }
+}
---
+++ Assets/Script/master/TrashBehavior_master.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TrashBehavior_master : MonoBehaviour {
+    
+    void Start()
+    {
+        Destroy(this.gameObject, 12.0f);
+        GetComponent<Animator>().speed = 0;
+        
+    }
+
+    public void Die()
+    {
+        GetComponent<BoxCollider2D>().enabled = false;
+        GetComponent<Animator>().speed = 5;
+        Destroy(this.gameObject, 1f);
+    }
+}
