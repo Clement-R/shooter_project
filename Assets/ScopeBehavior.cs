2cdfd32e6727291c3312e84290a8a5c8db5c18bf
Clement-R <rondeau.clement@gmail.com>
2016-12-01 09:49:56 +0100

    Remove useless lines

--- Assets/ScopeBehavior.cs
+++ Assets/ScopeBehavior.cs
@@ -2,13 +2,7 @@
 using System.Collections;
 
 public class ScopeBehavior : MonoBehaviour {
-    private Rigidbody2D rb2D;
-
-    void Start () {
-        rb2D = GetComponent<Rigidbody2D>();
-    }
-	
-	// Update is called once per frame
+    
 	void FixedUpdate () {
         var angle = Mathf.Atan2(Input.GetAxisRaw("Horizontal_2"), -Input.GetAxisRaw("Vertical_2")) * Mathf.Rad2Deg;
 
@@ -19,6 +13,5 @@ public class ScopeBehavior : MonoBehaviour {
 
         angle = Mathf.Clamp(angle, -90, 90);
         transform.rotation = Quaternion.Euler(0, 0, -angle);
-        // rb2D.MoveRotation(-angle);
     }
 }
