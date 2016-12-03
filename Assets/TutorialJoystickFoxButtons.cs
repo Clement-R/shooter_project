c5cefd893522e4f60aa6bb8d4621365bc1cbeef5
Clement-R <rondeau.clement@gmail.com>
2016-12-03 19:13:47 +0100

    Add new events

---
+++ Assets/TutorialJoystickFoxButtons.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TutorialJoystickFoxButtons : MonoBehaviour {
+    public float timeBeforeFail = 5.0f;
+
+    private float nextFail = 0.0F;
+    private EventBehavior eventManager;
+
+    void Start() {
+        eventManager = GetComponent<EventBehavior>();
+        nextFail = Time.time + timeBeforeFail;
+    }
+
+    void Update () {
+        if ((Input.GetButton("Fire_1_1") || Input.GetButton("Fire_1_2") || Input.GetButton("Fire_1_3") || Input.GetButton("Fire_1_4"))) {
+            eventManager.success = true;
+        }
+
+        if (Time.time > nextFail) {
+            nextFail = Time.time + timeBeforeFail;
+            eventManager.fail = true;
+        }
+    }
+}
