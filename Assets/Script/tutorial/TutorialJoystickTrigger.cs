2b197cc7a1a1e4cf48ef967e4b7a69d831dc16f4
Clement-R <rondeau.clement@gmail.com>
2016-12-04 15:22:58 +0100

    Re-add all prefabs and scripts for Events

---
+++ Assets/Script/tutorial/TutorialJoystickTrigger.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TutorialJoystickTrigger : MonoBehaviour {
+    public float timeBeforeFail = 5.0f;
+    [Tooltip("0 = FOX; 1 = GRANDMA")]
+    public int playerIndex = 0;
+    [Tooltip("0 = LEFT; 1 = RIGHT; 2 = LEFT & RIGHT")]
+    public int triggerToTest = 0;
+
+    private EventBehavior eventManager;
+    private float nextFail = 0.0F;
+
+    void Start() {
+        eventManager = GetComponent<EventBehavior>();
+        nextFail = Time.time + timeBeforeFail;
+    }
+
+    void Update () {
+        if (playerIndex == 0) {
+            if(triggerToTest == 0) {
+                if(Input.GetAxisRaw("Trigger_1_left") == 1) {
+                    eventManager.success = true;
+                }
+            } else if (triggerToTest == 1) {
+                if(Input.GetAxisRaw("Trigger_1_right") == 1) {
+                    eventManager.success = true;
+                }
+            } else if (triggerToTest == 2) {
+                if(Input.GetAxisRaw("Trigger_1_right") == 1 && Input.GetAxisRaw("Trigger_1_left") == 1) {
+                    eventManager.success = true;
+                }
+            }
+        } else {
+            if (triggerToTest == 0) {
+                if (Input.GetAxisRaw("Trigger_2_left") == 1) {
+                    eventManager.success = true;
+                }
+            }
+            else if (triggerToTest == 1) {
+                if (Input.GetAxisRaw("Trigger_2_right") == 1) {
+                    eventManager.success = true;
+                }
+            }
+            else if (triggerToTest == 2) {
+                if (Input.GetAxisRaw("Trigger_2_right") == 1 && Input.GetAxisRaw("Trigger_2_left") == 1) {
+                    eventManager.success = true;
+                }
+            }
+        }
+
+        if (Time.time > nextFail) {
+            nextFail = Time.time + timeBeforeFail;
+            eventManager.fail = true;
+        }
+    }
+}
---
+++ Assets/TutorialTrashTargeting.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TutorialTrashTargeting : MonoBehaviour {
+    public GameObject trash;
+
+    private EventBehavior eventManager;
+    private GameObject trashInstance;
+    private TrashBehavior_master behavior;
+
+	void Start () {
+        eventManager = GetComponent<EventBehavior>();
+        trashInstance = Instantiate(trash);
+        trashInstance.transform.position = new Vector3(3.5f, -9.0f, 0.0f);
+        trashInstance.GetComponent<EnemyMovement>().speed = 0.0f;
+        behavior = trashInstance.GetComponent<TrashBehavior_master>();
+    }
+	
+	void Update () {
+	    if(behavior.isTargeted) {
+            eventManager.success = true;
+        }
+
+        Time.timeScale = 1.0f;
+    }
+}
